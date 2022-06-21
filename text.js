a07fcd2b53ab595569d1197996470e169276547b
sigzer <82393336+sigzer@users.noreply.github.com>
2022-06-21 15:51:54 +0200

    Add files via upload

---
+++ text.js
@@ -0,0 +1,20 @@
+
+const newTab = () => {
+    window.open('https://www.facebook.com/beni.kaibas/')
+}
+
+const projectNewTab = () => {
+    window.open('https://github.com/sigzer')
+}
+
+const btn_one = () => {
+    window.open('https://github.com/sigzer')
+};
+
+const btn_two = () => {
+    window.open('page-two.html')
+}
+
+const phoneNewTab = () => {
+    alert('+36202298148')
+}
---
+++ text2.js
@@ -0,0 +1,18 @@
+let width = $(window).width(); 
+
+window.onscroll = function(){
+if ((width >= 900)){
+    if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        $("#middle").css("background-size","150% auto");
+    }else{
+        $("#middle").css("background-size","100% auto");        
+    }
+}};
+
+setTimeout(function(){
+    $("#loading").addClass("animated fadeOut");
+    setTimeout(function(){
+      $("#loading").removeClass("animated fadeOut");
+      $("#loading").css("display","none");
+    },800);
+},1450);
